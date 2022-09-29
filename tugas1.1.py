a5b25fc562c32607d8e079230868fb1abbb699c0
Kukuh2307 <Kukuhagung12@gmail.com>
2022-09-28 18:37:05 -1200

    menyelesaikan tugas nomor 1

---
+++ tugas1.1.py
@@ -0,0 +1,6 @@
+def kuadrat(bilangan,pangkat):
+   return bilangan ** pangkat
+
+bilangan = int(input('Masukkan bilangan: '))
+pangkat = int(input('Masukkan pangkat: '))
+print(kuadrat(bilangan,pangkat))
