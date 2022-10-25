e0969638c0bc70111e4d79f731fe683e057c3b0c
kukuh2307 <kukuhagung12@gmail.com>
2022-10-25 18:48:34 +0700

    menambahkan praktikum bab 4 numpy array

---
+++ TipeSet.py
@@ -0,0 +1,8 @@
+mySet = {1,2,3,4,3,2}
+print(mySet)
+
+#mySet = {1,2,[3,4]}
+
+mySet = set([1,2,3,2])
+print(mySet)
+
---
+++ latihan2.1.py
@@ -0,0 +1,7 @@
+data_list = []
+data_string = ''
+data_tuple = ()
+
+print(type(data_list))
+print(type(data_string))
+print(type(data_tuple))
